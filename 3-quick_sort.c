a7262cf729d2565b1f7e6998e2d6e0e1fc338cdb
EduNgaira <ngairaedwin@gmail.com>
2023-04-25 09:49:39 -0700

    Add files

---
+++ 3-quick_sort.c
@@ -0,0 +1,98 @@
+#include "sort.h"
+#include<stdio.h>
+
+/**
+ * swap_pos - swaps  valiue's position in array
+ *
+ * @array: array to be changed
+ * @first: first index
+ * @second: second index
+ */
+void swap_pos(int **array, size_t first, size_t second)
+{
+	int temp;
+
+	temp = (*array)[first];
+	(*array)[first] = (*array)[second];
+	(*array)[second] = temp;
+}
+
+/**
+ * partition - divides array into two parts
+ * lower and higher
+ *
+ * @array: array to be sorted
+ * @lower: lower boundary
+ * @upper: upper boundary
+ * @size: size of the array
+ *
+ * Return: index of sorted pivot
+ */
+size_t partition(int **array, size_t lower, size_t upper, size_t size)
+{
+	size_t lb, ub, pivot;
+
+	pivot = upper;
+	lb = lower;
+
+	for (ub = lb; ub < upper; ub++)
+	{
+		/* check if ub is lesser than pivot */
+		if ((*array)[ub] <= (*array)[pivot])
+		{
+			if (lb != ub)
+			{
+				swap_pos(array, lb, ub);
+				print_array(*array, size);
+			}
+			ub = ub + 1;
+		}
+	}
+
+	/* swap pivot to its original position */
+	if (ub != lb)
+	{
+		swap_pos(array, ub, lb);
+		print_array(*array, size);
+	}
+	return (lb);
+}
+/**
+ * sorter - sorts array recursively
+ *
+ * @array: array to be sorted
+ * @lb: lower bound
+ * @ub: upper bound
+ * @size: size of the array
+ */
+void sorter(int **array, size_t lb, size_t ub, size_t size)
+{
+	size_t sorted_index;
+
+	/* recursive breakpoint */
+	if (lb < ub && *array)
+	{
+		sorted_index = partition(array, lb, ub, size);
+
+		/* perform recursive sort */
+		if (sorted_index - lb > 1)	/* more than one element present */
+			sorter(array, lb, sorted_index - 1, size);    /* sorts lower boundary */
+
+		if (ub - sorted_index > 1)
+			sorter(array, sorted_index + 1, ub, size);    /* sort upper boundary */
+	}
+}
+
+/**
+ * quick_sort - applies quick sort to given array
+ *
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	/* create the sorter function */
+	sorter(&array, 0, size - 1, size);
+}
